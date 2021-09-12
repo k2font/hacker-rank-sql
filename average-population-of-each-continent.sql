45ba61b6a344b200a261d589819bd923c780348e
shoichiro <shoichiro.kouno@gmail.com>
2021-09-12 20:31:06 +0900

    Update average-population-of-each-continent.sql

--- average-population-of-each-continent.sql
+++ average-population-of-each-continent.sql
@@ -1,1 +1,2 @@
+# https://www.hackerrank.com/challenges/average-population-of-each-continent/problem
 select cou.continent, floor(avg(cit.population)) from city cit inner join country cou where cit.countrycode = cou.code group by cou.continent;
